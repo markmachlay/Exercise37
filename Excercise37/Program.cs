5e2a30b4a17893a725b6ae644809814d113ce7b5
markmachlay <84604813+markmachlay@users.noreply.github.com>
2021-06-14 15:38:23 -0400

    Add files via upload

---
+++ Excercise37/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Excercise37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char continueFlag;
+            do
+            {
+                int[] array = new int[5];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine("Please enter a number");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+
+                int sum;
+                sum = array.Sum();
+                Console.WriteLine($"The sum of is {array[0]}. {array[1]}, {array[2]}, {array[3]} and {array[4]}{sum}");
+
+                Console.WriteLine("Would you like to contune? (y/n)");
+                continueFlag = Console.ReadLine().ToLower()[0];
+            } while (continueFlag == 'y');
+        }
+    }
+}
